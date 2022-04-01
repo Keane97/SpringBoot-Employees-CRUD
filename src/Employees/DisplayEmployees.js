bb011f043c8b5cb9427ed4ee5bbe0cb3198097d5
Keane97 <liamkeane49@yahoo.com>
2022-04-01 16:32:47 +0100

    GetEmployeeByID + CSS
    Implemented the GetSingleEmployee functions which will retrieve the records for a single employee based off the record ID.
    
    Added some error catching to the image display (i.e. if the image is null, use the placeholder image instead.
    
    Applied some style to the cards and included an add option icon via react icons library
    
    Created the Update employee form. Leave this until after the "Add employee" functionality is done

--- src/Employees/DisplayEmployees.js
+++ src/Employees/DisplayEmployees.js
@@ -3,17 +3,20 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Card, Button } from 'react-bootstrap'
+import { FaUserPlus } from 'react-icons/fa';
 import "./DisplayEmployees.css";
 
 function DisplayEmployees() {
 
-    // const employeeData = { firstName: 'firstName', lastName: 'lastName', email: 'email', aboutMe: 'aboutMe' };
     const [employees, getEmployees] = useState('');
+    const [employee, getEmployee] = useState('');
     const URL = 'http://localhost:8080/api/';
 
     useEffect(() => {
         getAllEmployees();
+        getEmployee();
     }, [])
+
 
 
     const getAllEmployees = () => {
@@ -23,6 +26,15 @@ function DisplayEmployees() {
                 console.log(response.data);
                 getEmployees(response.data);
             }).catch(error => console.error(`Error: ${error}`));
+    }
+
+    const getSingleEmployeeData = (id) => {
+        axios
+            .get(`${URL}employees/6`) // Replace the 6 with the ID of the user in question. Currently only a placeholder
+            .then((response) => {
+                console.log(response.data);
+                getEmployee(response.data);
+            }).catch(error => console.error(`Invalid User ID: ${error}`));
     }
 
     //pass id through function to update the correct employee
@@ -37,31 +49,38 @@ function DisplayEmployees() {
     //         });
     // }
 
-
     return (
         <div className='Employee_display'>
 
-          
-                {employees ? employees.map(employeeData => {
-                    return (
-                        <div className='Display_Employee' key={employeeData.id}>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={require(`../Assets/images/${employeeData.image}`)} />
-                                <Card.Body>
-                                    <Card.Title>{employeeData.firstName} {employeeData.lastName}</Card.Title>
-                                    <Card.Text>{employeeData.aboutMe}</Card.Text>
-                                    <div className="card_footer">
-                                        <Button variant="primary">Edit Employee Details</Button>
-                                        <Button variant="danger" >Delete Employee Details</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                    )
-                }) : <h3>No data received</h3>}
+            {employees ? employees.map(employeeData => {
+                return (
+                    <div className='Display_Employee' key={employeeData.id}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={employeeData.image ? require(`../Assets/images/${employeeData.image}`) : require("../Assets/images/placeholder.png")} />
+                            <div className="card_body">
+
+                            </div>
+                            <Card.Body>
+                                <Card.Title>{employeeData.firstName} {employeeData.lastName}</Card.Title>
+                                <Card.Text >{employeeData.aboutMe}</Card.Text>
+                                <div className="card_footer">
+                                    <Button variant="primary">Edit Employee Details</Button>
+                                    <Button variant="danger" >Delete Employee Details</Button>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                )
+            }) : <h3>No data received</h3>}
+            <div className="new_Employee">
+                <div className="add_svg">
+                    <FaUserPlus size={150} color='green' onClick={() => getSingleEmployeeData()} />  {/* "getSingleEmployeeData is a placeholder yet. This is to route the user to the "Add Employee" form page */}
+                </div>
+
+            </div>
 
         </div>
-    )
+    ) //master return
 }
 
 export default DisplayEmployees
---
+++ src/Employees/UpdateEmployee.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function UpdateEmployee() {
+
+    return (
+        <form>
+            <label>First Name:</label>
+            <input type="text" />
+
+            <label>First Name:</label>
+            <input type="text" />
+
+            <label>Email:</label>
+            <input type="email" />
+
+            <label>About Me:</label>
+            <input type="text" />
+        </form>
+    )
+}
+
+export default UpdateEmployee
